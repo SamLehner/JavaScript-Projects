0382462c3f698df8769135db5a6acdc837906422
SamLehner <samblehner8@gmail.com>
2020-10-20 16:58:55 -0600

    First projects
    Main.js and basic index.html

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,10 @@
+
+var A = "I deleted all my code,\'So now i am retyping stuff\'"//This is code declaring the string value to A
++"This is a continued statement!";
+
+var B = "This is a second sentence"
+
+var A = A.fontcolor("red");//This is chagning ther font color to red
+document.write(A+B);
+
+window.alert(3+3)//A window alert for an expression
